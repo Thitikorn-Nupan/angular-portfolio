efc0e25345d01cb3c04fa0404b0656946217bc20
thitikorn_nupan@outlook.co.th <thitikorn_nupan@outlook.co.th>
2026-02-19 11:20:22 +0700

    change using css framework by CLI to install into project

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -13,10 +13,10 @@ export class ContactComponent {
   }[]
   constructor() {
     this.aLinks = [
-      { href : 'https://www.facebook.com/mr.thitikorn.nupan/' , faIcon : 'fa-facebook-f'},
-      { href : 'https://www.linkedin.com/in/thitikorn-nupan/' , faIcon : 'fa-linkedin-in'},
-      { href : 'https://github.com/thitikorn-nupan' , faIcon : 'fa-github'},
-      { href : 'assets/pdf/resume.pdf' , faIcon : 'fa-file'},
+      { href : 'https://www.facebook.com/mr.thitikorn.nupan/' , faIcon : 'https://img.icons8.com/?size=100&id=118497&format=png&color=000000'},
+      { href : 'https://www.linkedin.com/in/thitikorn-nupan/' , faIcon : 'https://img.icons8.com/?size=100&id=13930&format=png&color=000000'},
+      { href : 'https://github.com/thitikorn-nupan' , faIcon : 'https://img.icons8.com/?size=100&id=63777&format=png&color=000000'},
+      { href : 'assets/pdf/resume.pdf' , faIcon : 'https://img.icons8.com/?size=100&id=mcyAsTDJNTI9&format=png&color=000000'},
     ]
   }
 }
